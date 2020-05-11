481fd8e042f951f55bd7e7ab18987941bf8c957f
samarjit98 <karmakar.samarjit@gmail.com>
2020-05-11 22:54:14 +0530

    dataset

--- terminal_logs/plot_curves.py
+++ terminal_logs/plot_curves.py
@@ -1,15 +1,16 @@
 import os
 import pickle
 import matplotlib.pyplot as plt
+from scipy.ndimage.filters import gaussian_filter1d
 
 files = os.listdir('.')
 
 for f in files:
-	if '.pkl' in f:
+	if '.pkl' in f and 'loss' not in f:
 		file_handler = open(f, 'rb+')
 		train_acc, test_acc = pickle.load(file_handler)
-		train_acc = train_acc[:65]
-		test_acc = test_acc[:65]
+		train_acc = gaussian_filter1d(train_acc[:65], sigma=1)
+		test_acc = gaussian_filter1d(test_acc[:65], sigma=1)
 		file_handler.close()
 		train_x = list(range(1, len(train_acc) + 1))
 		test_x = list(range(1, len(test_acc) + 1))
@@ -20,5 +21,24 @@ for f in files:
 		plt.plot(test_x, test_acc, label = "Test Accuracy") 
 		plt.xlabel('Epoch')
 		plt.xlabel('Accuracy')
+		plt.legend()
+		plt.savefig(os.path.join("./", f[0:-4] + ".png"))
+
+	if '.pkl' in f and 'loss' in f:
+		file_handler = open(f, 'rb+')
+		train_loss, test_loss = pickle.load(file_handler)
+		file_handler.close()
+
+		train_loss = gaussian_filter1d(train_loss[:60], sigma=1)
+		test_loss = gaussian_filter1d(test_loss[:60], sigma=1)
+		train_x = list(range(1, len(train_loss) + 1))
+		test_x = list(range(1, len(test_loss) + 1))
+		plt.figure()
+		#plt.ylim(20, 110)
+		plt.ylim(0.4, 1.0)
+		plt.plot(train_x, train_loss, label = "Train Loss")
+		plt.plot(test_x, test_loss, label = "Test Loss") 
+		plt.xlabel('Epoch')
+		plt.xlabel('Loss')
 		plt.legend()
 		plt.savefig(os.path.join("./", f[0:-4] + ".png"))
--- terminal_logs/text_to_array.py
+++ terminal_logs/text_to_array.py
@@ -19,3 +19,26 @@ for f in files:
 		pickle.dump((train_acc, test_acc), file_handler)
 		file_handler.close()
 
+for f in files:
+	if ".txt" in f: 
+		print(f)
+		content = open(f, 'r').read()
+		content = [ x.split()[0][0:-1] for x in  content.split("Train Loss: ")][1:]
+		content = [float(x) for x in content]
+
+		train_loss = content
+		print(train_loss)
+		print(len(train_loss))
+
+		content1 = open(f, 'r').read()
+		content1 = [ x.split()[0][0:-1] for x in  content1.split("Test Loss: ")][1:]
+		content1 = [float(x) for x in content1]
+
+		test_loss = content1
+		print(test_loss)
+		print(len(test_loss))
+
+		file_handler = open(f[0:-4] + "_loss.pkl", 'wb+')
+		pickle.dump((train_loss, test_loss), file_handler)
+		file_handler.close()
+
